bca38fee3b8fe3ed7ff02bda74064dee0aee407a
atton <e115763@gmail.com>
2017-07-21 10:46:58 +0900

    Copying tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -2,8 +2,6 @@
 
 @section('content')
 
-    <!-- Bootstrap Boilerplate... -->
-
     <div class="panel-body">
         <!-- Display Validation Errors -->
         @include('common.errors')
@@ -32,5 +30,45 @@
         </form>
     </div>
 
-    <!-- TODO: Current Tasks -->
+    <!-- Current Tasks -->
+    @if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Current Tasks
+            </div>
+
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+
+                    <!-- Table Headings -->
+                    <thead>
+                        <th>Task</th>
+                        <th>&nbsp;</th>
+                    </thead>
+
+                    <!-- Table Body -->
+                    <tbody>
+                        @foreach ($tasks as $task)
+                        <tr>
+                                <!-- Task Name -->
+                                <td class="table-text">
+                                    <div>{{ $task->name }}</div>
+                                </td>
+
+                                <!-- Delete Button -->
+                                <td>
+                                    <form action="/task/{{ $task->id }}" method="POST">
+                                        {{ csrf_field() }}
+                                        {{ method_field('DELETE') }}
+
+                                        <button>Delete Task</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,39 @@
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
 
-Route::get('/', function() {
-    return view('tasks');
+
+Route::get('/', function () {
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
 });
 
-Route::post('/task', function(Request $request) {
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
 });
 
-Route::delete('/task/{id}', function($id) {
-});
+Route::delete('/task/{id}', function ($id) {
+    Task::findOrFail($id)->delete();
 
+    return redirect('/');
+});
 ?>
